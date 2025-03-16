69b8f0256c342247e0a7aaaf4105a63014da15b7
Mahito TANNO <mahito@presche.me>
2025-03-16 18:30:07 +0900

    chore:  use vite to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
+import build from "@hono/vite-build/node";
 import devServer from "@hono/vite-dev-server";
 import nodeAdapter from "@hono/vite-dev-server/node";
 
 export default defineConfig({
   plugins: [
+    build({
+      entry: "src/index.ts",
+    }),
     devServer({
       entry: "src/index.ts",
       adapter: nodeAdapter,
