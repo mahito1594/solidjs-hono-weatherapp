9c715cb9934de3843ae995540a0fca016b3da7d8
Mahito TANNO <mahito@presche.me>
2025-03-16 18:02:24 +0900

    chore:  use vite as dev server

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import devServer from "@hono/vite-dev-server";
+import nodeAdapter from "@hono/vite-dev-server/node";
+
+export default defineConfig({
+  plugins: [
+    devServer({
+      entry: "src/index.ts",
+      adapter: nodeAdapter,
+    })
+  ]
+});
